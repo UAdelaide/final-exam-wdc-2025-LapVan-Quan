fc88af22505eb0aa17f4f571f36b6234da63ad80
Lap Van Quan <a1895135@adelaide.edu.au>
2025-06-20 04:04:36 +0000

    [automatic commit] 2025-06-20 04:04:36 UTC

--- part1/app.js
+++ part1/app.js
@@ -94,7 +94,7 @@ app.get('/api/dogs', async (req, res) => {
       SELECT d.name AS dog_name, d.size, u.username AS owner_username
       FROM Dogs AS d
       JOIN Users AS u ON d.owner_id = u.user_id;
-      `;
+    `;
 
     const [dogs] = await db.query(sql);
     console.log(dogs)
@@ -108,12 +108,12 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
     const sql = `
-        SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
-        FROM WalkRequests wr
-        JOIN Dogs d ON wr.dog_id = d.dog_id
-        JOIN Users u ON d.owner_id = u.user_id
-        WHERE wr.status = 'open';
-        `;
+      SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
+      FROM WalkRequests wr
+      JOIN Dogs d ON wr.dog_id = d.dog_id
+      JOIN Users u ON d.owner_id = u.user_id
+      WHERE wr.status = 'open';
+    `;
 
   }
 
