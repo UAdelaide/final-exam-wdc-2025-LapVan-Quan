5909cb2dfd1323c7169f6a9518d8a0c83420bcf7
Lap Van Quan <a1895135@adelaide.edu.au>
2025-06-20 03:28:02 +0000

    [automatic commit] 2025-06-20 03:28:02 UTC

--- part1/app.js
+++ part1/app.js
@@ -61,7 +61,7 @@ let db;
       VALUES
       ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
       ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-      ((SELECT user_id FROM Users WHERE username = 'eve789'), 'Rocky', 'large'),
+      ((SELECT user_id FROM Users WHERE username = 'bobwalker'), 'Rocky', 'large'),
       ((SELECT user_id FROM Users WHERE username = 'jaden'), 'Milo', 'small'),
       ((SELECT user_id FROM Users WHERE username = 'john'), 'Luna', 'medium');
       `);
