d404fee1c74d15815c5948f1cb298f78ac820f5a
Lap Van Quan <a1895135@adelaide.edu.au>
2025-06-20 04:01:31 +0000

    [automatic commit] 2025-06-20 04:01:31 UTC

--- part1/app.js
+++ part1/app.js
@@ -105,7 +105,14 @@ app.get('/api/dogs', async (req, res) => {
 });
 
 app.get('/api/walkrequests/open', async(req, res) => {
-    
+    const sql = `
+    SELECT wr.request_id, d.name AS dog_name, wr.requested_time,
+    wr.duration_minutes, wr.location, u.username AS owner_username
+FROM WalkRequests wr
+JOIN Dogs d ON wr.dog_id = d.dog_id
+JOIN Users u ON d.owner_id = u.user_id
+WHERE wr.status = 'open'
+    `;
 });
 
 module.exports = app;
