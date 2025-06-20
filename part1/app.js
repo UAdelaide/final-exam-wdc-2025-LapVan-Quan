c2e5f64830e500479a80c6729f7d7ce44f6ea873
Lap Van Quan <a1895135@adelaide.edu.au>
2025-06-20 06:35:46 +0000

    [automatic commit] 2025-06-20 06:35:46 UTC

--- part1/app.js
+++ part1/app.js
@@ -134,20 +134,11 @@ app.get('/api/walkrequests/open', async (req, res) => {
 
 app.get('/api/walkers/summary', async (req, res) => {
   try {
+    const sql = `
+
+    `;
     const [summary] = await db.query(`
-      SELECT
-        u.username AS walker_username,
-        COUNT(wr.rating_id) AS total_ratings,
-        ROUND(AVG(wr.rating), 1) AS average_rating,
-        (
-          SELECT COUNT(*) FROM WalkRequests wrq
-          JOIN WalkApplications wa ON wrq.request_id = wa.request_id
-          WHERE wa.walker_id = u.user_id AND wrq.status = 'completed'
-        ) AS completed_walks
-      FROM Users u
-      LEFT JOIN WalkRatings wr ON u.user_id = wr.walker_id
-      WHERE u.role = 'walker'
-      GROUP BY u.user_id;
+
     `);
     res.json(summary);
   } catch (err) {
