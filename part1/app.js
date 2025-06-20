85ec6cd10dca3741f9710a214f9ffd28e152d3ec
Lap Van Quan <a1895135@adelaide.edu.au>
2025-06-20 03:15:23 +0000

    [automatic commit] 2025-06-20 03:15:23 UTC

--- part1/app.js
+++ part1/app.js
@@ -39,13 +39,16 @@ let db;
 
 
     // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
+    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM users');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+      INSERT INTO Users (username, email, password_hash, role)
+      VALUES
+      ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+      ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+      ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+      ('jaden', 'jaden@gmail.com', 'hashed321', 'walker'),
+      ('john', 'john@adelaide.com', 'hashed654', 'owner');
       `);
     }
   } catch (err) {
