8e3f2cf9266f6d9481d92b33308085e36e6c267b
Lap Van Quan <a1895135@adelaide.edu.au>
2025-06-20 20:55:29 +0000

    [automatic commit] 2025-06-20 20:55:29 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -21,17 +21,14 @@ router.get('/', async (req, res) => {
     res.json(rows);
   } catch (error) {
     console.error('SQL Error:', error);
-    res.status(500).json({ error: 'Failed to fetch walk requests' });
+    res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
 
 router.get('/all', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT d.dog_id, d.name AS dog_name, u.username AS owner_name
-      FROM Dogs d JOIN Users u
-      ON d.owner_id = u.user_id
-      WHERE u.user_id = ?;
+      SELECT * FROM Dogs;
     `, [user_id]);
 
     res.json(rows);
