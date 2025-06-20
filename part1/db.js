e330e0625e484428cd53e531350704f82282608e
Lap Van Quan <a1895135@adelaide.edu.au>
2025-06-20 03:50:35 +0000

    [automatic commit] 2025-06-20 03:50:35 UTC

--- part1/db.js
+++ part1/db.js
@@ -7,10 +7,5 @@ const db = mysql.createConnection({
     database: 'DogWalkService'
 });
 
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL');
-});
-
 module.exports = db;
 
