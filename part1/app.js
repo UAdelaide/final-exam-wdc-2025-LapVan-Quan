624027ef76aac330c054fcc46c9e98bcb5e12243
Lap Van Quan <a1895135@adelaide.edu.au>
2025-06-20 04:19:48 +0000

    [automatic commit] 2025-06-20 04:19:48 UTC

--- part1/app.js
+++ part1/app.js
@@ -146,3 +146,12 @@ app.get('/api/walkers/summary', async (req, res) => {
 });
 
 module.exports = app;
+
+INSERT INTO Users (username, email, password_hash, role)
+VALUES
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('jaden', 'jaden@gmail.com', 'hashed321', 'walker'),
+('john', 'john@adelaide.com', 'hashed654', 'owner');
+
